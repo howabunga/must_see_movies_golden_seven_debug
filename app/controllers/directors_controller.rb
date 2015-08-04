c7efc4b73f5779bad996b94dc47415358df45925
howabunga <weihowa@gmail.com>
2015-08-04 00:52:15 -0500

    directors and actors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,16 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
     @director = Director.find(params["id"])
+    render("show_details.html.erb")
+
   end
 
-  def new
+  def new_form
+
   end
 
   def create_row
@@ -19,7 +22,7 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("show_details.html.erb")
   end
 
   def edit_form
@@ -29,14 +32,14 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    render("show_details.html.erb")
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
   get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
   get('/create_director', { :controller => 'directors', :action => 'create_row' })
 
-  # READ
-  get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/:id', { :controller => 'directors', :action => 'show' })
-
   # UPDATE
   get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
   get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+
+    # READ
+  get('/directors', { :controller => 'directors', :action => 'index' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
 
   # DELETE
   get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
